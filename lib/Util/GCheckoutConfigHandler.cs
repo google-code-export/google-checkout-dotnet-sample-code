24a7ba1d86d6e489f3c6fc1c1a4fdff9db236597
joseph.feser <joseph.feser@localhost>
2007-07-20 03:27:03 +0000

    Added initial code for Proxy Support.
    This is to support Issue #4

--- lib/Util/GCheckoutConfigHandler.cs
+++ lib/Util/GCheckoutConfigHandler.cs
@@ -41,7 +41,8 @@ namespace GCheckout.Util {
     /// <param name="configContext"></param>
     /// <param name="section"></param>
     /// <returns></returns>
-    public object Create(object parent, object configContext, XmlNode section) {
+    public object Create(object parent, object configContext, 
+      XmlNode section) {
 
       GCheckoutConfigSection retVal = new GCheckoutConfigSection();
 
@@ -52,6 +53,11 @@ namespace GCheckout.Util {
       string currency = string.Empty;
       long platformID = 0;
       int envTemp = 0;
+      bool useProxy = false;
+      string proxyHost = string.Empty;
+      string proxyUserName = string.Empty;
+      string proxyPassword = string.Empty;
+
       EnvironmentType environment = EnvironmentType.Unknown;
 
       bool logging = false;
@@ -60,24 +66,31 @@ namespace GCheckout.Util {
 
       string logDirectory = string.Empty;
 
-      GCheckoutConfigurationHelper.GetStringValue(section, "SandboxMerchantID", ref sandboxMerchantID);
-      GCheckoutConfigurationHelper.GetStringValue(section, "SandboxMerchantKey", ref sandboxMerchantKey);
-      GCheckoutConfigurationHelper.GetStringValue(section, "ProductionMerchantID", ref productionMerchantID);
-      GCheckoutConfigurationHelper.GetStringValue(section, "ProductionMerchantKey", ref productionMerchantKey);
-      GCheckoutConfigurationHelper.GetStringValue(section, "Currency", false, ref currency);
+      GCheckoutConfigurationHelper.GetStringValue(
+        section, "SandboxMerchantID", ref sandboxMerchantID);
+      GCheckoutConfigurationHelper.GetStringValue(
+        section, "SandboxMerchantKey", ref sandboxMerchantKey);
+      GCheckoutConfigurationHelper.GetStringValue(
+        section, "ProductionMerchantID", ref productionMerchantID);
+      GCheckoutConfigurationHelper.GetStringValue(
+        section, "ProductionMerchantKey", ref productionMerchantKey);
+      GCheckoutConfigurationHelper.GetStringValue(
+        section, "Currency", false, ref currency);
 
-
-      GCheckoutConfigurationHelper.GetEnumValue(section, "Environment", typeof(EnvironmentType), ref envTemp);
+      GCheckoutConfigurationHelper.GetEnumValue(
+        section, "Environment", typeof(EnvironmentType), ref envTemp);
       environment = (EnvironmentType) envTemp;
 
-      GCheckoutConfigurationHelper.GetStringValue(section, "Logging", false, ref logTemp);
+      GCheckoutConfigurationHelper.GetStringValue(
+        section, "Logging", false, ref logTemp);
       if (logTemp != null && logTemp.Length > 0) {
         logging = logTemp.ToLower() == "true";
       }
 
       //ensure we do not throw an exception.
       try {
-        GCheckoutConfigurationHelper.GetStringValue(section, "PlatformID", false, ref platformIDTemp);
+        GCheckoutConfigurationHelper.GetStringValue(
+          section, "PlatformID", false, ref platformIDTemp);
         if (platformIDTemp != null && platformIDTemp.Length > 0) {
           platformID = long.Parse(platformIDTemp);
         }
@@ -86,13 +99,15 @@ namespace GCheckout.Util {
         throw new ConfigurationException("Error Setting PlatformID", ex);
       }
 
-      GCheckoutConfigurationHelper.GetStringValue(section, "LogDirectory", false, ref logDirectory);
+      GCheckoutConfigurationHelper.GetStringValue(
+        section, "LogDirectory", false, ref logDirectory);
 
       try {
         retVal.ProductionMerchantID = long.Parse(productionMerchantID);
       }
       catch (Exception ex) {
-        throw new ConfigurationException("Error Setting ProductionMerchantID", ex);
+        throw
+          new ConfigurationException("Error Setting ProductionMerchantID", ex);
       }
       retVal.ProductionMerchantKey = productionMerchantKey;
 
@@ -100,7 +115,46 @@ namespace GCheckout.Util {
         retVal.SandboxMerchantID = long.Parse(sandboxMerchantID);
       }
       catch (Exception ex) {
-        throw new ConfigurationException("Error Setting SandboxMerchantID", ex);
+        throw 
+          new ConfigurationException("Error Setting SandboxMerchantID", ex);
+      }
+
+      //try to read the UseProxy Key.
+      try {
+        string useProxyVal = null;
+        GCheckoutConfigurationHelper.GetStringValue(
+          section, "UseProxy", false, ref useProxyVal);
+        if (useProxyVal != null && useProxyVal.Length > 0) {
+          useProxy = bool.Parse(useProxyVal);   
+        }
+      }
+      catch (Exception ex) {
+        throw new ConfigurationException("Error Setting UseProxy", ex);
+      }
+
+      //if we have the key, then attempt to read the other values
+      //if true then all other values are required
+      //someday someone may request us to support a blank password, 
+      //but we are not going to support that
+      //since it is a security issue.
+      if (useProxy) {
+        GCheckoutConfigurationHelper.GetStringValue(
+          section, "ProxyHost", ref proxyHost);
+
+        //what we are going to do is attempt to validate the uri of the proxy
+        if (proxyHost != string.Empty) {
+          try {
+            Uri proxyUrl = new Uri(proxyHost);
+          }
+          catch (Exception ex) {
+            throw new ConfigurationException("Error Setting ProxyHost", ex);
+          }
+        }
+
+        GCheckoutConfigurationHelper.GetStringValue(
+          section, "ProxyUserName", ref proxyUserName);
+        GCheckoutConfigurationHelper.GetStringValue(
+          section, "ProxyPassword", ref proxyPassword);
       }
 
       retVal.SandboxMerchantKey = sandboxMerchantKey;
@@ -109,6 +163,10 @@ namespace GCheckout.Util {
       retVal.LogDirectory = logDirectory;
       retVal.PlatformID = platformID;
       retVal.Currency = currency;
+      retVal.UseProxy = useProxy;
+      retVal.ProxyHost = proxyHost;
+      retVal.ProxyUserName = proxyUserName;
+      retVal.ProxyPassword = proxyPassword;
 
       return retVal;
     }
